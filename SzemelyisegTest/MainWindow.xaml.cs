7982981159e902e7ac13c7d9d7cd48767a9b745e
TothZsofia <tzsofia07@gmail.com>
2024-03-18 11:18:18 +0100

    :D

--- SzemelyisegTest/MainWindow.xaml.cs
+++ SzemelyisegTest/MainWindow.xaml.cs
@@ -24,8 +24,34 @@ namespace SzemelyisegTest
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MessageBoxResult gomb = MessageBox.Show("Ügyes vagy!:3", "ablak", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-           
-            
+
+            foreach (CheckBox checkbox in stc1.Children)
+            {
+                if (checkbox.IsChecked == true)
+                {
+                    lbl1.Items.Add(checkbox.Content);
+                }
+            }
+
+
+            foreach (CheckBox checkbox in stc2.Children)
+            {
+                if (checkbox.IsChecked == true)
+                {
+                    lbl2.Items.Add(checkbox.Content);
+                }
+
+            }
+            lbl3.Content= ((ComboBoxItem)combo1.SelectedItem).Content;
+
+            foreach (RadioButton radioButton in stc3.Children)
+            {
+                if (radioButton.IsChecked == true)
+                {
+                    lbl4.Content = radioButton.Content;
+                }
+            }
+            lbl5.Content = ((ListBoxItem)list1.SelectedItem).Content;
         }
     }
 }
